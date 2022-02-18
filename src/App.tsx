f49497397565e885d209dfbfa31f4cdbffb90962
Yarchyk <yarykomm@gmail.com>
2022-02-18 10:52:14 +0200

    CSS, Image, Typeface - config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./style.css";
+import ReactLogo from "./react-logo.png";
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <div>Hello!</div>
+      <img src={ReactLogo} alt="react-logo" width="300" />
+    </>
+  );
 };
 
 export default App;
