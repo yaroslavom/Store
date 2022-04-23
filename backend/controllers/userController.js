91fb3ffebc278637b6d870bc4bdc510e97ce03b9
Yarchyk <yarykomm@gmail.com>
2022-04-23 15:41:11 +0300

    User auth & generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
 import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import User from "../models/userModel.js"
+import generatedToken from "../utils/generateTokens.js";
 
 // @desc   Auth user & get token
 // @route   POST /api/users/login
 // @desc   Public
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.send({ email, password });
+  const user = await User.findOne({ email })
+
+  if(user && (await user.matchPassword(password))) {
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generatedToken(user._id),
+    })
+  } else {
+      res.status(401)
+      throw new Error('invalid email or password')
+  }
 });
