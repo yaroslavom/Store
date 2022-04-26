2ca7e0bfc240e5b6c0065e5d104290cde964ee29
Yarchyk <yarykomm@gmail.com>
2022-04-26 13:56:05 +0300

    Backend User Registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,16 +8,49 @@ import generatedToken from "../utils/generateTokens.js";
 export const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
-  if(user) {
+  if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-    })
+    });
   } else {
     res.status(404);
-    throw new Error('User not found')
+    throw new Error("User not found");
+  }
+});
+
+// @desc   register new user
+// @route   GET /api/users
+// @desc   Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existUser = await User.findOne({ email });
+
+  if (existUser) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generatedToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 });
 
